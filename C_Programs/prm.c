c6475816587e31fa8410f665c7b0cc0b7b1ad0e6
roshanthakre <roshan6895@gmail.com>
2021-10-05 01:08:50 +0530

    roshanthakre

---
+++ C_Programs/prm.c
@@ -0,0 +1,15 @@
+/*this is my first C program
+this is called as first C comment*/
+#include<stdio.h>
+#include<canio.h>
+/*globel variation declaration*/
+int main()
+{
+
+printf("welcome to india\n");
+printf("wecome to mkpits");
+return 0;
+
+}
+
+
